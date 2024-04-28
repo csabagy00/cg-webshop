b766e9c43519dc4bb227b7ea77838dc683935b9c
csabagy00 <csaba.gyo00@gmail.com>
2024-04-28 19:11:40 +0200

    Created a jsx for the main page.

---
+++ cgWebShop/frontend/src/Pages/MainPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Components/Header";
+
+
+function MainPage(){
+
+
+  return(
+    <>
+      <Header />
+      <div>
+        <p>Main</p>
+      </div>
+    </>
+  )
+}
+
+export default MainPage;
