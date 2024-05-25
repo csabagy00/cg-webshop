4756e46f6ca9b1fad72f8691d2a9512153fa69bd
csabagy00 <csaba.gyo00@gmail.com>
2024-05-25 22:33:31 +0200

    Added Authentication and DbContext to services, configured swagger, added Roles to DB and 1 Admin user.

--- cgWebShop/cgWebShopApi/Program.cs
+++ cgWebShop/cgWebShopApi/Program.cs
@@ -1,24 +1,53 @@
+using System.Text;
 using cgWebShopApi.Controllers;
 using cgWebShopApi.Data;
 using cgWebShopApi.Models;
 using cgWebShopApi.Respositories;
+using cgWebShopApi.Services.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+IConfigurationRoot configurationRoot = configurationBuilder.AddUserSecrets<Program>().Build();
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
-builder.Services.AddScoped<NpgsqlConnection>();
+builder.Services.AddScoped<AuthenticationSeeder>();
+//builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
 //builder.Services.AddSingleton<IProductsRepository, ProductsRepository>();
 
+/////Authentication/////
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var validIssuer = configurationRoot["AppSettings:ValidIssuer"];
+        var validAudience = configurationRoot["AppSettings:ValidAudience"];
+        var secretValue = configurationRoot["AppSettings:IssuerSigningKey"];
+        
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = validIssuer,
+            ValidAudience = validAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(secretValue!))
+        };
+    });
 
+
+/////IdentityCore/////
 builder.Services.AddIdentityCore<AppUser>(options =>
     {
         options.SignIn.RequireConfirmedAccount = false;
@@ -32,7 +61,23 @@ builder.Services.AddIdentityCore<AppUser>(options =>
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<CgShopContext>();
 
+/////DbContext/////
+builder.Services.AddDbContext<CgShopContext>(options =>
+{
+    options.UseNpgsql(configurationRoot["ConnectionString"]);
+});
+
+ConfigureSwagger();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var authenticationSeeder = scope.ServiceProvider.GetRequiredService<AuthenticationSeeder>();
+    authenticationSeeder.AddRoles();
+    authenticationSeeder.AddAdmin();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -44,7 +89,39 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
 app.Run();
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
+}
