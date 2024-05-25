6ae026f13d36a0b51b83ca909bd42d25acf19c22
csabagy00 <csaba.gyo00@gmail.com>
2024-05-25 21:25:28 +0200

    Removed Id property

--- cgWebShop/cgWebShopApi/Models/AppUser.cs
+++ cgWebShop/cgWebShopApi/Models/AppUser.cs
@@ -4,7 +4,6 @@ namespace cgWebShopApi.Models;
 
 public class AppUser : IdentityUser
 {
-    public int Id { get; set; }
     public string FirstName { get; set; }
     public string? MiddleName { get; set; }
     public string LastName { get; set; }
