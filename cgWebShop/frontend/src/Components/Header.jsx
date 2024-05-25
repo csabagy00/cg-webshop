a374e1e2f0502547a45ca963aa06c61920f0a1dd
csabagy00 <csaba.gyo00@gmail.com>
2024-05-25 20:47:00 +0200

    Added UseEffect and new divs.

--- cgWebShop/frontend/src/Components/Header.jsx
+++ cgWebShop/frontend/src/Components/Header.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from 'react'
 import "./css/Header.css"
-
 
 function Header(){
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch()
+    }
+  }, [])
+
   return (
     <nav className="navbar">
-      <div className='main'>NavBar</div>
+      <div className="nav-content">
+        <div className='dropdown'>
+          <button className="btn">Categories</button>
+          <div className='dd-content'>
+            <a></a>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
