bec81a84d57dfafdb46654e2d836caa9b042d601
csabagy00 <csaba.gyo00@gmail.com>
2024-05-25 20:49:59 +0200

    Implemented Order model class

---
+++ cgWebShop/cgWebShopApi/Models/Order.cs
@@ -0,0 +1,13 @@
+namespace cgWebShopApi.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public Product Product { get; set; }
+    public AppUser AppUser { get; set; }
+    public DateTime Date { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public int PostalCode { get; set; }
+}
