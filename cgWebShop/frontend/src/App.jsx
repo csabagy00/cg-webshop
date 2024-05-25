532474767d733fdad7fb7c13cd31fc4945b904aa
csabagy00 <csaba.gyo00@gmail.com>
2024-05-25 20:47:38 +0200

    Added Header component

--- cgWebShop/frontend/src/App.jsx
+++ cgWebShop/frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './Pages/MainPage'
+import Header from './Components/Header'
+import './App.css'
 
 function App() {
  
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />}/>
       </Routes>
