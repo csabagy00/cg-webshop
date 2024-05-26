6cbcf78e5ca350f580581071dfc6914b6bd57ef6
csabagy00 <csaba.gyo00@gmail.com>
2024-05-26 20:23:03 +0200

    Refactored Repository to use dbContext.

--- cgWebShop/cgWebShopApi/Respositories/Product/ProductsRepository.cs
+++ cgWebShop/cgWebShopApi/Respositories/Product/ProductsRepository.cs
@@ -1,140 +1,104 @@
 using System.Data;
+using cgWebShopApi.Data;
 using cgWebShopApi.Models;
+using Microsoft.EntityFrameworkCore;
 using Npgsql;
 
 namespace cgWebShopApi.Respositories;
 
 public class ProductsRepository : IProductsRepository
 {
-    private NpgsqlConnection _connection;
-    private Category[] _categories;
+    private readonly CgShopContext _dbContext;
+    private readonly ILogger<ProductsRepository> _logger;
 
-    public ProductsRepository(NpgsqlConnection connection)
+    public ProductsRepository(CgShopContext dbContext, ILogger<ProductsRepository> logger)
     {
-        _connection = connection;
-        _categories = (Category[])Enum.GetValues(typeof(Category));
+        _dbContext = dbContext;
+        _logger = logger;
     }
         
     
     public async Task<List<Product>> GetAllProducts()
     {
-        await _connection.OpenAsync();
-        var adapter = new NpgsqlDataAdapter("SELECT * FROM products", _connection);
-
-        var dataSet = new DataSet();
-        await Task.Run(() => adapter.Fill(dataSet));
-        
-        var table = dataSet.Tables[0];
-
-        var queryResult = new List<Product>();
-
-        foreach (DataRow row in table.Rows)
+        try
         {
-            Console.WriteLine((int)row["product_id"]);
-
-            var product = new Product
-            {
-                Id = (int)row["product_id"],
-                Name = (string)row["product_name"],
-                Category = _categories[(int)row["category_id"] - 1],
-                InStock = (int)row["in_stock"],
-                Price = (int)row["price"],
-                Img = row["img"] is System.DBNull ? null : (string)row["img"]
-            };
-            
-            queryResult.Add(product);
+            return await _dbContext.Products
+                .Include(p => p.Category)
+                .ToListAsync();
         }
-
-        await _connection.CloseAsync();
-        
-        return queryResult;
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
     }
 
 
     public async Task<Product> GetProductById(int id)
     {
-        await _connection.OpenAsync();
-        var adapter = new NpgsqlDataAdapter($"SELECT * FROM products WHERE product_id = {id}", _connection);
-
-        var dataSet = new DataSet();
-        await Task.Run(() => adapter.Fill(dataSet));
-
-        var table = dataSet.Tables[0];
-
-        var row = table.Rows[0];
-
-        var product = new Product
+        try
         {
-            Id = (int)row["product_id"],
-            Name = (string)row["product_name"],
-            Category = _categories[(int)row["category_id"] - 1],
-            InStock = (int)row["in_stock"],
-            Price = (int)row["price"],
-            Img = row["img"] is System.DBNull ? null : (string)row["img"]
-        };
-        
-        await _connection.CloseAsync();
-
-        return product;
+            return await _dbContext.Products.Include(p => p.Category).FirstOrDefaultAsync(p => p.Id == id);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
     }
 
     public async Task AddNewProduct(Product product)
     {
-        await _connection.OpenAsync();
-
-        string query = $"INSERT INTO products (category_id, product_name, in_stock, price, img) VALUES" +
-                       $"(@CategoryId, @ProductName, @InStock, @Price, @Img)";
-        
-        await using (NpgsqlCommand command = new NpgsqlCommand(query, _connection))
+        try
         {
-            command.Parameters.AddWithValue("@CategoryId", Array.IndexOf(Enum.GetValues(typeof(Category)), product.Category));
-            command.Parameters.AddWithValue("@ProductName", product.Name);
-            command.Parameters.AddWithValue("@InStock", product.InStock);
-            command.Parameters.AddWithValue("@Price", product.Price);
-            command.Parameters.AddWithValue("@Img", string.IsNullOrEmpty(product.Img) ? DBNull.Value : product.Img);
-            
-            await command.ExecuteNonQueryAsync();
+            _dbContext.Entry(product.Category).State = EntityState.Unchanged;
+            await _dbContext.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
         }
-
-        await _connection.CloseAsync();
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
     }
 
     public async Task DeleteOneProduct(int id)
     {
-        await _connection.OpenAsync();
-
-        string query = $"DELETE FROM products WHERE product_id = @Id";
-
-        await using (NpgsqlCommand command = new NpgsqlCommand(query, _connection))
+        try
         {
-            command.Parameters.AddWithValue("@Id", id);
-
-            await command.ExecuteNonQueryAsync();
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                await _dbContext.SaveChangesAsync();
+            }
         }
-
-        await _connection.CloseAsync();
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
     }
 
-    public async Task UpdateProductById(int id, Product product)
+    public async Task UpdateProductById(Product product)
     {
-        await _connection.OpenAsync();
-
-        string query = $"UPDATE products SET category_id = @CategoryId, product_name = @Name, in_stock = @InStock, " +
-                       $"price = @Price, img = @Img WHERE product_id = @ProductId";
-
-        await using (NpgsqlCommand command = new NpgsqlCommand(query, _connection))
+        try
         {
-            command.Parameters.AddWithValue("@CategoryId", Array.IndexOf(Enum.GetValues(typeof(Category)), product.Category));
-            command.Parameters.AddWithValue("@Name", product.Name);
-            command.Parameters.AddWithValue("@InStock", product.InStock);
-            command.Parameters.AddWithValue("@Price", product.Price);
-            command.Parameters.AddWithValue("@Img", string.IsNullOrEmpty(product.Img) ? DBNull.Value : product.Img);
-            command.Parameters.AddWithValue("@ProductId", id);
+            var exsitingProduct = await _dbContext.Products.FindAsync(product.Id);
+            if (exsitingProduct != null)
+            {
+                _dbContext.Entry(exsitingProduct).CurrentValues.SetValues(product);
+                if (product.Category != null)
+                    _dbContext.Entry(exsitingProduct.Category).State = EntityState.Unchanged;
 
-            await command.ExecuteNonQueryAsync();
+                await _dbContext.SaveChangesAsync();
+            }
         }
-
-        await _connection.CloseAsync();
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
     }
     
 }
