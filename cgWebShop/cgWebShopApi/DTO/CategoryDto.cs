a4bff8c1bba95c9525622041fbf83a1c947242ee
csabagy00 <csaba.gyo00@gmail.com>
2024-06-01 16:22:35 +0200

    Removed ICollection from CategoryDto

--- cgWebShop/cgWebShopApi/DTO/CategoryDto.cs
+++ cgWebShop/cgWebShopApi/DTO/CategoryDto.cs
@@ -3,6 +3,4 @@ namespace cgWebShopApi.DTO;
 public class CategoryDto
 {
     public string Name { get; set; }
-
-    public ICollection<ProductDto> Products { get; set; } = new List<ProductDto>();
 }
