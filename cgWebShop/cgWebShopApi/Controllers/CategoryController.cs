954cc917139cc0a3e9a11a5b9d66918fc0e4d63f
csabagy00 <csaba.gyo00@gmail.com>
2024-06-01 16:22:15 +0200

    Implemented Post and Delete endpoint for CategoryController

--- cgWebShop/cgWebShopApi/Controllers/CategoryController.cs
+++ cgWebShop/cgWebShopApi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using cgWebShopApi.DTO;
 using cgWebShopApi.Models;
 using cgWebShopApi.Respositories.Category;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -40,11 +41,50 @@ public class CategoryController : ControllerBase
         try
         {
             var category = await _categoryRepository.GetCategoryById(id);
-            
+
             if (category == null)
                 return NotFound();
-            
+
             return Ok(category);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Category>> PostNewCategory([FromBody] CategoryDto categoryDto)
+    {
+        try
+        {
+            Category category = new Category { Name = categoryDto.Name };
+
+            await _categoryRepository.AddNewCategory(category);
+
+            return Ok(category);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<int>> DeleteCategory(int id)
+    {
+        try
+        {
+            var category = await _categoryRepository.GetCategoryById(id);
+
+            if (category.Products.Count > 0)
+                return Conflict();
+
+            await _categoryRepository.Deletecategory(category);
+
+            return NoContent();
         }
         catch (Exception e)
         {
