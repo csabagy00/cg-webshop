c48fd72e9c99620b565b51829558afd4b11f7ff9
Csaba <csaba.gyo00@gmail.com>
2024-06-08 13:17:02 +0200

    Implemented fetch and display of products

--- cgWebShop/frontend/src/Pages/MainPage.jsx
+++ cgWebShop/frontend/src/Pages/MainPage.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../Components/ProductCard";
 import Header from "../Components/Header";
 
 
 function MainPage(){
+  const [products, setProducts] = useState();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/Products');
+        const result = await response.json();
+        setProducts(result);
+        
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchData();
+  }, [])
 
   return(
     <>
-      <div>
-        <p>Main</p>
+      <div className="main-products">
+        {products && products.map(p => {
+          return(
+            <ProductCard product={p}/>
+          )
+        })}
       </div>
     </>
   )
