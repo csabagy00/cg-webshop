592b6333a3fa7fc1a79381bff4e613d9dfc0004c
Csaba <csaba.gyo00@gmail.com>
2024-06-08 13:17:43 +0200

    Implemented dropdown categories menu

--- cgWebShop/frontend/src/Components/Header.jsx
+++ cgWebShop/frontend/src/Components/Header.jsx
@@ -5,12 +5,21 @@ import "./css/Header.css"
 
 function Header({ isAuthenticated, setIsAuthenticated }){
   var navigate = useNavigate();
-  
+  const [categories, setCategories] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch()
+      try {
+        const response = await fetch('/api/Category')
+        const result = await response.json()
+        setCategories(result)
+
+      } catch (error) {
+        console.error(error)
+      }
     }
+
+    fetchData();
   }, [])
 
   const logout = () => {
@@ -26,7 +35,16 @@ function Header({ isAuthenticated, setIsAuthenticated }){
     <nav className="navbar">
       <div className="nav-content">
         <button className='btn' onClick={() => navigate("/")}>Home</button>
-        <button className="btn">Categories</button>
+        <div className='dropdown'>
+          <button className='btn'>Categories</button>
+          <div className='dropdown-content'>
+            {categories && categories.map(c => {
+              return(
+                <button className='dropdown-btn'>{c.name}</button>
+              )
+            })}
+          </div>
+        </div>
         {
           !isAuthenticated ? 
           <>
