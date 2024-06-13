c88296c20742a58b05ddaac090532b466f6d0913
csabagy00 <csaba.gyo00@gmail.com>
2024-06-13 19:17:30 +0200

    Added Route to account page

--- cgWebShop/frontend/src/App.jsx
+++ cgWebShop/frontend/src/App.jsx
@@ -5,12 +5,15 @@ import Header from './Components/Header'
 import './App.css'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
+import Account from './Pages/Account'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [products, setProducts] = useState();
   const [filteredProducts, setFilteredProducts] = useState();
   const [searchValue, setSearchValue] = useState(null);
+
+  const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
 
   return (
     <BrowserRouter>
@@ -25,6 +28,7 @@ function App() {
         <Route path='/' element={<MainPage products={products} filteredProducts={filteredProducts} setProducts={setProducts} searchValue={searchValue}/>}/>
         <Route path='/login' element={<Login setIsAuthenticated={setIsAuthenticated}/>}/>
         <Route path='/register' element={<Register />}/>
+        <Route path='/account' element={<Account user={user} setIsAuthenticated={setIsAuthenticated}/>}/>
       </Routes>
     </BrowserRouter>
   )
