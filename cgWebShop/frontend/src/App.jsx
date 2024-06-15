81c6491f3930e50983a9f96da5d25ef64dd63f20
csabagy00 <csaba.gyo00@gmail.com>
2024-06-15 12:01:35 +0200

    Added properties

--- cgWebShop/frontend/src/App.jsx
+++ cgWebShop/frontend/src/App.jsx
@@ -8,10 +8,15 @@ import Register from './Pages/Register'
 import Account from './Pages/Account'
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const cartArray = [];
+  localStorage.setItem("cart", JSON.stringify(cartArray))
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [products, setProducts] = useState();
   const [filteredProducts, setFilteredProducts] = useState();
   const [searchValue, setSearchValue] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [cart, setCart] = useState(cartArray);
 
   const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
 
@@ -22,13 +27,23 @@ function App() {
         setFilteredProducts={setFilteredProducts} 
         setSearchValue={setSearchValue} 
         filteredProducts={filteredProducts}
-        products={products}/>
+        products={products}
+        setIsAdmin={setIsAdmin}/>
 
       <Routes>
-        <Route path='/' element={<MainPage products={products} filteredProducts={filteredProducts} setProducts={setProducts} searchValue={searchValue}/>}/>
-        <Route path='/login' element={<Login setIsAuthenticated={setIsAuthenticated}/>}/>
+        <Route path='/' element={<MainPage 
+          products={products} 
+          filteredProducts={filteredProducts} 
+          setProducts={setProducts} 
+          searchValue={searchValue} 
+          cartArray={cartArray} 
+          setCart={setCart}
+          cart={cart}
+          isAuthenticated={isAuthenticated}/>}/>
+
+        <Route path='/login' element={<Login setIsAuthenticated={setIsAuthenticated} setIsAdmin={setIsAdmin}/>}/>
         <Route path='/register' element={<Register />}/>
-        <Route path='/account' element={<Account user={user} setIsAuthenticated={setIsAuthenticated}/>}/>
+        <Route path='/account' element={<Account user={user} setIsAuthenticated={setIsAuthenticated} setIsAdmin={setIsAdmin} isAdmin={isAdmin} cartArray={cartArray}/>}/>
       </Routes>
     </BrowserRouter>
   )
