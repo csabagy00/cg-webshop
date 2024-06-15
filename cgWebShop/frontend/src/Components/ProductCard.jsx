e05d61e019c05e91abc8b505f0d23e95c3e82732
csabagy00 <csaba.gyo00@gmail.com>
2024-06-15 12:04:43 +0200

    Added button to ProductCard

--- cgWebShop/frontend/src/Components/ProductCard.jsx
+++ cgWebShop/frontend/src/Components/ProductCard.jsx
@@ -1,6 +1,7 @@
 import './css/ProductCard.css'
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, addToCart, isAuthenticated, setOpenModal }) => {
+
 
   return(
     <>
@@ -8,8 +9,8 @@ const ProductCard = ({ product }) => {
         <img src="../public/images/no-img.jpg" className="card-img"/>
         <div className="card-content">
           <h2>{product.name}</h2>
-          <p>{product.category.name}</p>
           <p>{product.price}</p>
+          <button className='btn' onClick={isAuthenticated ? () => addToCart(product) : () => setOpenModal(true)}>Add to Cart</button>
         </div>
       </div>
     </>
