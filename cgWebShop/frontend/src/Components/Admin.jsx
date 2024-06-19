2b9701d38585c7941f7d049c25656f9d60d697dc
csabagy00 <csaba.gyo00@gmail.com>
2024-06-19 19:04:26 +0200

    Added admin component/css for Account page

---
+++ cgWebShop/frontend/src/Components/Admin.jsx
@@ -0,0 +1,13 @@
+import './css/Admin.css'
+
+const Admin = () => {
+  return(
+    <div className='admin-container'>
+      <div className='admin-options'>
+        <p>Admin page</p>
+      </div>
+    </div>
+  )
+}
+
+export default Admin
