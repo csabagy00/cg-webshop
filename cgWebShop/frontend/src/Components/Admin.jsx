a5a0e7ea2e4ba0bf59871ea4988b3ba6e97b4440
csabagy00 <csaba.gyo00@gmail.com>
2024-07-11 12:33:37 +0200

    Added buttons to admin subpage + ternary

--- cgWebShop/frontend/src/Components/Admin.jsx
+++ cgWebShop/frontend/src/Components/Admin.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react'
 import './css/Admin.css'
+import CategoryForm from './CategoryForm'
+import ProductForm from './ProductForm';
 
 const Admin = () => {
+  const [showAdmin, setShowAdmin] = useState()
+
   return(
     <div className='admin-container'>
       <div className='admin-options'>
-        <p>Admin page</p>
+        <button onClick={() => setShowAdmin("addProduct")}>Add New Product</button>
+        <button onClick={() => setShowAdmin("addCategory")}>Add New Category</button>
+        <button>Remove Product</button>
+        <button>Remove Category</button>
+        <button>Add an Admin account</button>
       </div>
+      {showAdmin === "addCategory" ? 
+        <CategoryForm />
+      : showAdmin === "addProduct" ?
+        <ProductForm />
+      :
+        <></>
+      }
     </div>
   )
 }
