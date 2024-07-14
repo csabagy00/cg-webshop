2f9495d6d723d69bc99b260e5a6cb26ad1abb8ff
csabagy00 <csaba.gyo00@gmail.com>
2024-07-14 11:51:12 +0200

    Added onClick to remove buttons

--- cgWebShop/frontend/src/Components/Admin.jsx
+++ cgWebShop/frontend/src/Components/Admin.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './css/Admin.css'
 import CategoryForm from './CategoryForm'
 import ProductForm from './ProductForm';
+import RemoveCategory from './RemoveCategory';
+import RemoveProduct from './RemoveProduct';
 
 const Admin = () => {
   const [showAdmin, setShowAdmin] = useState()
@@ -11,16 +13,20 @@ const Admin = () => {
       <div className='admin-options'>
         <button onClick={() => setShowAdmin("addProduct")}>Add New Product</button>
         <button onClick={() => setShowAdmin("addCategory")}>Add New Category</button>
-        <button>Remove Product</button>
-        <button>Remove Category</button>
+        <button onClick={() => setShowAdmin("removeProduct")}>Remove Product</button>
+        <button onClick={() => setShowAdmin("removeCategory")}>Remove Category</button>
         <button>Add an Admin account</button>
       </div>
       {showAdmin === "addCategory" ? 
         <CategoryForm />
       : showAdmin === "addProduct" ?
         <ProductForm />
+      : showAdmin === "removeCategory" ?
+        <RemoveCategory />
+      : showAdmin === "removeProduct" ?
+        <RemoveProduct/>
       :
-        <></>
+        <></> 
       }
     </div>
   )
