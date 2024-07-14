4debe514581121f220c56a88a8dd26b4038a5abf
csabagy00 <csaba.gyo00@gmail.com>
2024-07-14 18:37:18 +0200

    Added a Modal to RemoveCategory

--- cgWebShop/frontend/src/Components/Admin.jsx
+++ cgWebShop/frontend/src/Components/Admin.jsx
@@ -4,9 +4,12 @@ import CategoryForm from './CategoryForm'
 import ProductForm from './ProductForm';
 import RemoveCategory from './RemoveCategory';
 import RemoveProduct from './RemoveProduct';
+import Modal from './Modal';
+import AddAdmin from './AddAdmin';
 
 const Admin = () => {
-  const [showAdmin, setShowAdmin] = useState()
+  const [showAdmin, setShowAdmin] = useState();
+  const [showCategoryModal, setShowCategoryModal] = useState(false);
 
   return(
     <div className='admin-container'>
@@ -15,18 +18,25 @@ const Admin = () => {
         <button onClick={() => setShowAdmin("addCategory")}>Add New Category</button>
         <button onClick={() => setShowAdmin("removeProduct")}>Remove Product</button>
         <button onClick={() => setShowAdmin("removeCategory")}>Remove Category</button>
-        <button>Add an Admin account</button>
+        <button onClick={() => setShowAdmin("addAdmin")}>Add an Admin account</button>
       </div>
       {showAdmin === "addCategory" ? 
         <CategoryForm />
       : showAdmin === "addProduct" ?
         <ProductForm />
       : showAdmin === "removeCategory" ?
-        <RemoveCategory />
+        <>
+        <RemoveCategory setShowCategoryModal={setShowCategoryModal}/>
+        <Modal isOpen={showCategoryModal} onClose={() => setShowCategoryModal(false)}>
+           <h2>Cannot delete category because there are products in this category</h2>
+        </Modal> 
+        </>
       : showAdmin === "removeProduct" ?
-        <RemoveProduct/>
+        <RemoveProduct/>   
+      : showAdmin === "addAdmin" ?
+        <AddAdmin />
       :
-        <></> 
+        <></>
       }
     </div>
   )
