69508320f5d5f20b04c0f7a9f35ee894900d96fe
csabagy00 <csaba.gyo00@gmail.com>
2024-07-14 18:40:14 +0200

    Created the component for adding/creating admin account + testing the layout

---
+++ cgWebShop/frontend/src/Components/AddAdmin.jsx
@@ -0,0 +1,17 @@
+import './css/AddAdmin.css'
+
+const AddAdmin = () => {
+  return(
+    <div className="addAdmin-container">
+      <div className="addAdmin-top">
+        <p className='addAdmin-top-left'>Add Admin Account</p>
+        <p className='addAdmin-top-right'>add admin test</p>
+      </div>
+      <div className="addAdmin-bottom">
+        <p>TEST</p>
+      </div>
+    </div>
+  )
+}
+
+export default AddAdmin
