c86fefb372013ba58619fc53e5d21a01e6d2e2a5
csabagy00 <csaba.gyo00@gmail.com>
2024-07-14 11:56:36 +0200

    Implemented Remove Category on Admin page

---
+++ cgWebShop/frontend/src/Components/RemoveCategory.jsx
@@ -0,0 +1,53 @@
+import { useContext } from "react"
+import { Context } from "../App"
+
+const RemoveCategory = () => {
+
+const { categories, products, setCategoriesRefresh, categoriesRefresh } = useContext(Context);
+
+function calculateTotal(id){
+  return products.filter(p => p.category.id === id).length
+}
+
+const deleteCategory = async (id) => {
+  try {
+    const response = await fetch(`/api/Category?id=${id}`, {
+      method: 'DELETE'
+    })
+
+    setCategoriesRefresh(!categoriesRefresh)
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Total product</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {categories && categories.map((c) => {
+          return (
+            <tr key={c.id}>
+              <td>{c.id}</td>
+              <td>{c.name}</td>
+              <td>{calculateTotal(c.id)}</td>
+              <td>
+                <button onClick={() => deleteCategory(c.id)}>Remove</button>
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default RemoveCategory
