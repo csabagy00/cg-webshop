0b5cbcdf04152b29fb81e86c4c819c18f6c744d0
csabagy00 <csaba.gyo00@gmail.com>
2024-07-19 11:00:23 +0200

    Created UserUpdateDTO

---
+++ cgWebShop/cgWebShopApi/DTO/UserUpdateDto.cs
@@ -0,0 +1,12 @@
+namespace cgWebShopApi.DTO;
+
+public class UserUpdateDto
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public string First { get; set; }
+    public string Middle { get; set; }
+    public string Last { get; set; }
+    public string Username { get; set; }
+    public string Phone { get; set; }
+}
