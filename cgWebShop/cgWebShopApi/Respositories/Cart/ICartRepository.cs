4bce906663ffaa94f190f580ab8334c000ccce78
csabagy00 <csaba.gyo00@gmail.com>
2024-07-23 17:43:58 +0200

    Implemented CartRepository

---
+++ cgWebShop/cgWebShopApi/Respositories/Cart/ICartRepository.cs
@@ -0,0 +1,11 @@
+using cgWebShopApi.Models;
+
+namespace cgWebShopApi.Respositories.Cart;
+
+public interface ICartRepository
+{
+    public Task<Models.Cart> GetCartByUserId(string id);
+    public Task<Models.Cart> AddNewCart(string email);
+
+    public Task<CartItem> AddNewCartItemToCart(Product product, string userId, int quantity);
+}
