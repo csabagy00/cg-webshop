890c191caf42e1cf0eeb053059d2283d9e23a997
csabagy00 <csaba.gyo00@gmail.com>
2024-07-23 14:17:16 +0200

    CartUpdate migrations

---
+++ cgWebShop/cgWebShopApi/Migrations/20240723120748_CartUpdate3.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cgWebShopApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CartUpdate3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartItems",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CartId",
+                table: "CartItems",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CartId",
+                table: "CartItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
