0d29ceec8541f62b1b69ae3047d6c9547a17279a
csabagy00 <csaba.gyo00@gmail.com>
2024-08-05 16:54:05 +0200

    Changed show state to showAcc state

--- cgWebShop/frontend/src/Pages/Account.jsx
+++ cgWebShop/frontend/src/Pages/Account.jsx
@@ -8,11 +8,10 @@ import Admin from '../Components/Admin';
 import Orders from './Orders';
 
 const Account = () => {
-  const [show, setShow] = useState(null);
   const [ordersRefresh, setOrdersRefresh] = useState(false)
   const navigate = useNavigate();
 
-  const { user, setIsAuthenticated, setIsAdmin, isAdmin, cart, setCart} = useContext(Context);
+  const { setShowAcc, showAcc, user, setIsAuthenticated, setIsAdmin, isAdmin, cart, setCart} = useContext(Context);
  
   const logout = () => {
     localStorage.removeItem("user")
@@ -30,22 +29,22 @@ const Account = () => {
         <div className='acc-h2-bg'>
           <h2 id='acc-fullname'>{user.first + (user.middle ? " " + user.middle + " " : " ") + user.last}</h2>
         </div>
-        <button onClick={() => setShow("cart")}>Cart</button>
-        <button onClick={() => setShow("details")}>Details</button>
-        <button onClick={() => setShow("orders")}>Orders</button>
-        { isAdmin ? <button onClick={() => setShow("admin")}>Admin page</button> : <></>}
+        <button onClick={() => setShowAcc("cart")}>Cart</button>
+        <button onClick={() => setShowAcc("details")}>Details</button>
+        <button onClick={() => setShowAcc("orders")}>Orders</button>
+        { isAdmin ? <button onClick={() => setShowAcc("admin")}>Admin page</button> : <></>}
         <button onClick={logout}>Logout</button>
       </div>
-        { show == "cart" ?
+        { showAcc == "cart" ?
           <Cart cart={cart} navigate={navigate} setOrdersRefresh={setOrdersRefresh} user={user} setCart={setCart}/>
         : 
-        show == "details" ? 
+        showAcc == "details" ? 
           <Details user={user}/>
         :
-        show == "admin" ? 
+        showAcc == "admin" ? 
           <Admin /> 
         :
-        show == "orders" ?
+        showAcc == "orders" ?
           <Orders user={user} />
         :
         <></>
