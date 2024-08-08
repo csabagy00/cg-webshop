eb604f2b866aef90b27d7595921e208b118649ad
csabagy00 <csaba.gyo00@gmail.com>
2024-08-08 13:12:18 +0200

    Added class name and onClick property to products name

--- cgWebShop/frontend/src/Components/ProductCard.jsx
+++ cgWebShop/frontend/src/Components/ProductCard.jsx
@@ -1,13 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import './css/ProductCard.css'
 
 const ProductCard = ({ key, product, addToCart, isAuthenticated, setOpenModal }) => {
+  const navigate = useNavigate();
 
+  function nameConverter(name){
+    return name.replace(/ /g, '-')
+  }
 
   return(
       <div className="prod-card">
         <img src={ product.img ? product.img : "../public/images/no-img.jpg"} className="card-img"/>
         <div className="card-content">
-          <h2>{product.name}</h2>
+          <h2 className='prod-name' onClick={() => navigate(`/products/${nameConverter(product.name)}`)}>{product.name}</h2>
           <p>{product.price}</p>
           <button className='btn' onClick={isAuthenticated ? () => addToCart(product) : () => setOpenModal(true)}>Add to Cart</button>
         </div>
