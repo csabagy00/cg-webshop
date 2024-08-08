a17d4bd2e06642b0af28403c178f9ad4ba02a7eb
csabagy00 <csaba.gyo00@gmail.com>
2024-08-08 14:26:28 +0200

    Added Products in the same category

--- cgWebShop/frontend/src/Pages/ProductInfo.jsx
+++ cgWebShop/frontend/src/Pages/ProductInfo.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import { useParams } from "react-router-dom"
 import './css/ProductInfo.css'
 import ImagePopup from "../Components/ImagePopup";
+import SmallProductCard from "../Components/SmallProductCard";
+import Modal from "../Components/Modal";
 
-const ProductInfo = ({ products, addToCart }) => {
+const ProductInfo = ({ products, addToCart, isAuthenticated }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
 
   const { productName } = useParams();
 
@@ -23,23 +26,44 @@ const ProductInfo = ({ products, addToCart }) => {
     }
   }
 
+  const filtered = products.filter(p => p.category.name === product.category.name && p.name !== product.name);
+
   return(
-    <div className="product-info">
-      <div className="product-inner">
-        <div className="product-img" onClick={() => setIsOpen(true)}>
-          <img src={ product.img ? product.img : "../public/images/no-img.jpg"} className="product-img"/>
+    <>
+      <div className="product-info">
+        <div className="product-inner">
+          <div className="product-img" onClick={() => setIsOpen(true)}>
+            <img src={ product.img ? product.img : "../public/images/no-img.jpg"} className="product-img"/>
+          </div>
+          <div className="product-detail">
+            <div>
+              <h3 className="product-p">{product.name}</h3>
+              <p className="product-p">{product.category.name}</p>
+              <p className="product-p">{product.price} €</p>
+            </div>
+            <div className="field-post-cart">
+              <input type="button" value="Add to Cart" onClick={isAuthenticated ? () => addToCart(product) : () => setOpenModal(true)}/>
+            </div>
+          </div>
         </div>
-        <div className="product-detail">
-          <h3 className="product-p">{product.name}</h3>
-          <p className="product-p">{product.category.name}</p>
-          <p className="product-p">{product.price} €</p>
-        </div>
-        <div className="field-post-cart">
-          <input type="button" value="Add to Cart" onClick={() => addToCart(product)}/>
-        </div>
+        <ImagePopup src={product.img ? product.img : "../public/images/no-img.jpg"} isOpen={isOpen} onClose={() => setIsOpen(false)}/>
       </div>
-      <ImagePopup src={product.img ? product.img : "../public/images/no-img.jpg"} isOpen={isOpen} onClose={() => setIsOpen(false)}/>
-    </div>
+      <h4>Products in the same category</h4>
+      <div className="similar">    
+        { filtered.length > 0 ? 
+          filtered.map(fp => {
+          console.log(fp);
+          return(
+            <SmallProductCard product={fp}/>
+          )
+        }) :
+        <h3>No more product in this category</h3>
+        }
+      </div>
+      <Modal isOpen={openModal} onClose={() => setOpenModal(false)}>
+        <h3>Please log in to continue</h3>
+      </Modal>
+    </>
   )
 }
 
